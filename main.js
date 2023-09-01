f46decc04ed960e63fea7a021dd4abd25a8eeb94
Clifford Gardner <cliffordgardner9@gmail.com>
2023-09-01 16:55:05 -0500

    Need to finish Filter

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict"
-
+//IS CALLED TO CREATE THE OUTLINE FOR EACH COFFEE PRODUCT
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee col-4" id="coffee" style="list-style: none">';
+    html += '<h1 style="font-size: 30px" class="coffeeName">' + coffee.name + '</h1>';
+    html += '<h3 class="coffeeRoast">' + coffee.roast + '</h3>';
+    html += '</div>';
 
     return html;
 }
-
+//CREATES THE HTML CODE THAT WILL BE FILTERED INTO THE DISPLAY FOR COFFEE
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -55,3 +54,51 @@ var roastSelection = document.querySelector('#roast-selection');
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+var search = document.getElementById("search")
+var roastselect = document.getElementById("roast-selection")
+//Filters Roast TODO: Filter out results based on values
+roastselect.addEventListener("change",
+    (event)=>{
+    switch (roastSelection.value){
+        case "All":
+            alert("All");
+            break;
+        case "1":
+            alert("1");
+            break;
+        case "2":
+            alert("2");
+            break;
+        case "3":
+            alert("3");
+            break;
+    }
+    })
+//Searches Coffee Input
+search.addEventListener("keyup", (event)=>{
+        let text = document.getElementById("coffee")
+        let coffeeproduct = document.getElementsByClassName("coffee")
+        let input = document.getElementById("search").value
+        input=input.toLowerCase()
+
+
+
+
+
+        let noResults = true;
+        for (let i = 0; i < coffeeproduct.length; i++) {
+            if (!coffeeproduct[i].innerHTML.toLowerCase().includes(input)) {
+                coffeeproduct[i].style.display="none";
+                continue;
+            }
+            else {
+                coffeeproduct[i].style.display="block";
+                noResults = false;
+            }
+        }
+})
+
+// document.addEventListener('keydown',hi)
+// input1.addEventListener("keydown", (event)=>{
+//     console.log(event.key)
+// })
